1baaf487ec3959e9b43dd460794a644c5304b25c
Xuefei Yan <saberbud@Xuefei-mac.lan>
2017-12-28 12:10:00 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Submit by saberbud.
+
---
+++ cachematrix.R
@@ -0,0 +1,35 @@
+## saberbud's inverse matrix cache functions
+
+## construct a special quantity by inputting a simple matrix.
+
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
+}
+
+
+## Find the inverse matrix from cache if it has been calculated.
+## Otherwise, calculate the inverse matrix.
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
+}
+
